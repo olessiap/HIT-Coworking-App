b3b03db004a330be1d0220daa58b277946368f3c
olessiap <olessia.potapova@gmail.com>
2020-02-12 23:23:15 -0300

    added CreatePost  component that takes content and image and renders it below

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   return (
     <div>
       <Header user={user} setUser={setUser}/>
+      <CreatePost />
     </div>
   )
 }
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
 function CreatePost() {
+  const [ content, setContent ] = React.useState("")
+  const [ image, setImage ] = React.useState(null)
+  
   return(
-    <div>CreatePost</div>
+    <>
+      <h1>Create a Post</h1>
+      <form>
+        <input 
+          type="text"
+          placeholder="enter content here"
+          onChange={event => setContent(event.target.value)}
+        />
+        <input 
+          type="file"
+          onChange={event => setImage(event.target.files[0])}
+        />
+        <button type="submit">Submit Post</button>
+      </form>
+      <p>{content}</p>
+      {image && 
+        <img 
+          src={URL.createObjectURL(image)}
+          style={{ height:300, width:200, objectFit:'cover' }}
+        />}
+    </>
   )
 }
 
