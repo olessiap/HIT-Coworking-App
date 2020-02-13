f698c11104186436bed06fcb7db4ffa1b4ae640f
olessiap <olessia.potapova@gmail.com>
2020-02-12 22:28:52 -0300

    added dynamic browser tab title

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import CreatePost from "./components/CreatePost"
 import Header from "./components/Header"
 import Login from "./components/Login"
-import Post from "./components/Post"
-import PostList from "./components/PostList"
 
 function App() {
   const [ user, setUser ] = React.useState("oli")
   
+  React.useEffect(() => {
+    document.title =  user ? `${user}'s feed` : "please log in"
+  }, [user])
+
   if(!user) {
     return <Login setUser={setUser}/>
   }
